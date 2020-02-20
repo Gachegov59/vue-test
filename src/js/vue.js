42f4d23f6b7a03a198fd6833824ba49e17f5cd7d
aleks.g <aleks.g@digital-spectr.ru>
2020-02-20 09:47:23 +0500

    test| lesson 1

--- src/js/vue.js
+++ src/js/vue.js
@@ -2,11 +2,37 @@ $(function () {
     var app = new Vue({
         el: '#vue',
         data: {
-            list: ['один', 'два', 'три'],
-            users: [
-                {id: 1, name: 'Ivan'},
-                {id: 2, name: 'Pety'}
-            ]
+            hashtags: [],
+            cities: [],
+            textSearch: '',
+            url: {
+                hashtags: 'https://dka-develop.ru/api?type=hashtag',
+                cities: 'https://dka-develop.ru/api?type=city'
+            }
+        },
+        watch: {
+           textSearch: function() {
+               if(this.textSearch.length > 3) {
+                   this.textSearch = 'Меняем содержимое поля из кода';
+               }
+           }
+        },
+        created: function () {
+            console.log(this.url.hashtags)
+        },
+        methods: {
+            getHashtags() {
+                axios.get(this.url.hashtags).then((response) => {
+                    console.log(this)
+                    this.hashtags = response.data;
+                })
+            },
+            getCities() {
+                axios.get(this.url.cities).then((response) => {
+                    console.log(response.data)
+                    this.cities = response.data;
+                })
+            }
         }
     })
 })
